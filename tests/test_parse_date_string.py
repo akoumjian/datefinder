569296a5e062a6304d748e33a570769e0fea3f58
Alec Koumjian <akoumjian@gmail.com>
2016-02-13 21:15:25 -0800

    fix alignment, fix requirements

--- tests/test_parse_date_string.py
+++ tests/test_parse_date_string.py
@@ -12,44 +12,50 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 @pytest.mark.parametrize('date_string, expected_parse_arg, expected_captures, expected_date', [
-    ('due on Tuesday Jul 22, 2014 eastern standard time',
-    'tuesday jul 22 2014',
-    {'timezones': ['eastern']},
-    datetime(2014, 7, 22).replace(tzinfo=tz.gettz('EST'))
+    (
+        'due on Tuesday Jul 22, 2014 eastern standard time',
+        'tuesday jul 22 2014',
+        { 'timezones': ['eastern'] },
+        datetime(2014, 7, 22).replace(tzinfo=tz.gettz('EST'))
     ),
-    ('Friday pAcific stanDard time',
-    'friday',
-    {'timezones':['pacific']},
-    parser.parse('friday').replace(tzinfo=tz.gettz('PST'))
+    (
+        'Friday pAcific stanDard time',
+        'friday',
+        { 'timezones': ['pacific'] },
+        parser.parse('friday').replace(tzinfo=tz.gettz('PST'))
     ),
-    ('13/03/2014 Central Daylight Savings Time',
-    '13/03/2014',
-    {'timezones':['central']},
-    datetime(2014, 3, 13).replace(tzinfo=tz.gettz('CST'))
+    (
+        '13/03/2014 Central Daylight Savings Time',
+        '13/03/2014',
+        { 'timezones': ['central'] },
+        datetime(2014, 3, 13).replace(tzinfo=tz.gettz('CST'))
     ),
     # assert dateparser.parse successfully
     # handles tz-naive date strings
     # and returns naive datetime objects
-    (' on 12/24/2015 at 2pm ',
-    '12/24/2015 at 2pm',
-    {'timezones':[]},
-    datetime(2015, 12, 24, 14, 0)
+    (
+        ' on 12/24/2015 at 2pm ',
+        '12/24/2015 at 2pm',
+        { 'timezones': [] },
+        datetime(2015, 12, 24, 14, 0)
     ),
     # test a tz abbreviation that
     # dateutile.tz.gettz cannot find
     # and will return None for
-    (' on 11-20-2015 6pm CST ',
-     '11-20-2015 6pm',
-     {'timezones':['CST']},
-     datetime(2015, 11, 20, 18, 0)
+    (
+        ' on 11-20-2015 6pm CST ',
+        '11-20-2015 6pm',
+        { 'timezones': ['CST'] },
+        datetime(2015, 11, 20, 18, 0)
     ),
     # test a tz abbreviation that
     # dateutile.tz.gettz cannot find
     # and will return None for
-    (' on 11-20-2015 6am IRST ',
-     '11-20-2015 6am',
-     {'timezones':['IRST']},
-     datetime(2015, 11, 20, 6, 0)
+    (
+        ' on 11-20-2015 6am IRST ',
+        '11-20-2015 6am',
+        { 'timezones': ['IRST'] },
+        datetime(2015, 11, 20, 6, 0)
     )
 ])
 def test_parse_date_string_find_replace(date_string, expected_parse_arg, expected_captures, expected_date):
