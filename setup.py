662baccb2b98cfd517bd6762ba995e09a9b7a051
alex <reddalexx@gmail.com>
2018-09-17 15:55:29 +0300

    update version to 0.6.2 in setup file

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.1',
+    version='0.6.2',
 
     description='Extract datetime objects from strings',
     long_description=long_description,
